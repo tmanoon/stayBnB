dadf0556d2ee942019a4bf019a615a05abee1f3a
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-07-18 15:52:30 +0300

    Add spaces and edit text to app footer component

--- frontend/src/cmps/AppFooter.jsx
+++ frontend/src/cmps/AppFooter.jsx
@@ -1,33 +1,35 @@
 export function AppFooter() {
 
-    const navigateTo = (url) => {
+    const navigateTo = url => {
         window.open(url, '_blank')
     }
-    
-    return <section className="app-footer-container">
-        <div className="app-footer">
 
-            <div className="options">
-                © 2024 Staybnb, Inc.
-                <div>·</div>
-                <span>TermsSitemap</span>
-                ·
-                <span>Privacy</span>
-                ·
-                <span>Your Privacy Choices</span>
-            </div>
+    return (
+        <section className="app-footer-container">
+            <div className="app-footer">
 
-            <div className="media">
-                <span> English (US)</span>
-                <span>$ USD</span>
-
-                <div className="share">
-                    <div className="facebook" onClick={() => navigateTo("https://www.facebook.com/airbnb/")}></div>
-                    <div className="twitter" onClick={() => navigateTo("https://twitter.com/airbnb")}></div>
-                    <div className="instagram" onClick={() => navigateTo("https://www.instagram.com/airbnb/")}></div>
+                <div className="options">
+                    © 2024 Staybnb, Inc.
+                    <div>·</div>
+                    <span>TermsSitemap</span>
+                    ·
+                    <span>Privacy</span>
+                    ·
+                    <span>Your Privacy Choices</span>
                 </div>
-            </div>
 
-        </div>
-    </section>
+                <div className="media">
+                    <span> English (US)</span>
+                    <span>$USD</span>
+
+                    <div className="share">
+                        <div className="facebook" onClick={() => navigateTo("https://www.facebook.com/airbnb/")}></div>
+                        <div className="twitter" onClick={() => navigateTo("https://twitter.com/airbnb")}></div>
+                        <div className="instagram" onClick={() => navigateTo("https://www.instagram.com/airbnb/")}></div>
+                    </div>
+                </div>
+
+            </div>
+        </section>
+    )
 }
