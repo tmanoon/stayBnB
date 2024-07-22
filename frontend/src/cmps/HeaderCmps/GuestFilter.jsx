bc055aa92af90689abdb9e986192b90017668ff5
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-07-23 00:20:40 +0300

    Edit logic of function in guest filter component

--- frontend/src/cmps/HeaderCmps/GuestFilter.jsx
+++ frontend/src/cmps/HeaderCmps/GuestFilter.jsx
@@ -1,11 +1,9 @@
-export function GuestFilter({ filterBy }) {
+export function GuestFilter({ filterByToEdit, setFilterByToEdit }) {
 
     function updateGuestCounts(guestType, countChange) {
-        const newGuestCounts = { ...filterBy.guestCount }
-
-        newGuestCounts[guestType] += countChange
-
-        setStayHeaderFilter({ ...filterBy, guestCount: newGuestCounts })
+        const newGuestCount = { ...filterByToEdit.guestCount }
+        newGuestCount[guestType] += countChange
+        setFilterByToEdit({ ...filterByToEdit, guestCount: newGuestCount })
     }
 
     return (
@@ -17,15 +15,15 @@ export function GuestFilter({ filterBy }) {
                         <span>Age 18 or above</span>
                     </div>
                     <div className="count">
-                        <button className={filterBy.guestCount.adults === 0 ? 'disabled' : ''} onClick={() => {
-                            if (filterBy.guestCount.adults > 1) {
+                        <button className={filterByToEdit.guestCount.adults === 0 ? 'disabled' : ''} onClick={() => {
+                            if (filterByToEdit.guestCount.adults > 1) {
                                 updateGuestCounts('adults', -1)
                             }
                         }}>-</button>
-                        {filterBy.guestCount.adults}
-                        <button className={filterBy.guestCount.adults + filterBy.guestCount.children >= 16 ? 'disabled' : ''} onClick={() => {
-                            if (filterBy.guestCount.adults + filterBy.guestCount.children < 16) {
-                                updateGuestCounts('adults', +1)
+                        {filterByToEdit.guestCount.adults}
+                        <button className={filterByToEdit.guestCount.adults + filterByToEdit.guestCount.children >= 16 ? 'disabled' : ''} onClick={() => {
+                            if (filterByToEdit.guestCount.adults + filterByToEdit.guestCount.children < 16) {
+                                updateGuestCounts('adults', 1)
                             }
                         }}>+</button>
                     </div>
@@ -36,14 +34,14 @@ export function GuestFilter({ filterBy }) {
                         <span>Ages 2 - 12</span>
                     </div>
                     <div className="count">
-                        <button className={filterBy.guestCount.children === 0 ? 'disabled' : ''} onClick={() => {
-                            if (filterBy.guestCount.children > 0) {
+                        <button className={filterByToEdit.guestCount.children === 0 ? 'disabled' : ''} onClick={() => {
+                            if (filterByToEdit.guestCount.children > 0) {
                                 updateGuestCounts('children', -1)
                             }
                         }}>-</button>
-                        {filterBy.guestCount.children}
-                        <button className={filterBy.guestCount.adults + filterBy.guestCount.children >= 16 ? 'disabled' : ''} onClick={() => {
-                            if (filterBy.guestCount.adults + filterBy.guestCount.children < 16) {
+                        {filterByToEdit.guestCount.children}
+                        <button className={filterByToEdit.guestCount.adults + filterByToEdit.guestCount.children >= 16 ? 'disabled' : ''} onClick={() => {
+                            if (filterByToEdit.guestCount.adults + filterByToEdit.guestCount.children < 16) {
                                 updateGuestCounts('children', +1)
                             }
                         }}>+</button>
@@ -55,14 +53,14 @@ export function GuestFilter({ filterBy }) {
                         <span>Under 2</span>
                     </div>
                     <div className="count">
-                        <button className={filterBy.guestCount.infants === 0 ? 'disabled' : ''} onClick={() => {
-                            if (filterBy.guestCount.infants > 0) {
+                        <button className={filterByToEdit.guestCount.infants === 0 ? 'disabled' : ''} onClick={() => {
+                            if (filterByToEdit.guestCount.infants > 0) {
                                 updateGuestCounts('infants', -1)
                             }
                         }}>-</button>
-                        {filterBy.guestCount.infants}
-                        <button className={filterBy.guestCount.infants === 5 ? 'disabled' : ''} onClick={() => {
-                            if (filterBy.guestCount.infants < 5) {
+                        {filterByToEdit.guestCount.infants}
+                        <button className={filterByToEdit.guestCount.infants === 5 ? 'disabled' : ''} onClick={() => {
+                            if (filterByToEdit.guestCount.infants < 5) {
                                 updateGuestCounts('infants', +1)
                             }
                         }}>+</button>
@@ -73,14 +71,14 @@ export function GuestFilter({ filterBy }) {
                         Pets
                     </div>
                     <div className="count">
-                        <button className={filterBy.guestCount.pets === 0 ? 'disabled' : ''} onClick={() => {
-                            if (filterBy.guestCount.pets > 0) {
+                        <button className={filterByToEdit.guestCount.pets === 0 ? 'disabled' : ''} onClick={() => {
+                            if (filterByToEdit.guestCount.pets > 0) {
                                 updateGuestCounts('pets', -1)
                             }
                         }}>-</button>
-                        {filterBy.guestCount.pets}
-                        <button className={filterBy.guestCount.pets === 5 ? 'disabled' : ''} onClick={() => {
-                            if (filterBy.guestCount.pets < 5) {
+                        {filterByToEdit.guestCount.pets}
+                        <button className={filterByToEdit.guestCount.pets === 5 ? 'disabled' : ''} onClick={() => {
+                            if (filterByToEdit.guestCount.pets < 5) {
                                 updateGuestCounts('pets', +1)
                             }
                         }}>+</button>
