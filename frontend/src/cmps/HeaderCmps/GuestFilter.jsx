6457be3f0933e8612d316d41c54ca784800a8f0e
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-07-22 23:57:13 +0300

    Remove extra spaces

--- frontend/src/cmps/HeaderCmps/GuestFilter.jsx
+++ frontend/src/cmps/HeaderCmps/GuestFilter.jsx
@@ -1,100 +1,95 @@
-import { store } from '../../store/store.js'
-
-export function GuestFilter({ filterByToEdit, setFilterByToEdit }) {
+export function GuestFilter({ headerFilterBy }) {
 
     function updateGuestCounts(guestType, countChange) {
-        const newGuestCounts = { ...filterByToEdit.guestCount }
+        const newGuestCounts = { ...headerFilterBy.guestCount }
 
         newGuestCounts[guestType] += countChange
 
-        setFilterByToEdit({ ...filterByToEdit, guestCount: newGuestCounts })
+        setStayHeaderFilter({ ...headerFilterBy, guestCount: newGuestCounts })
     }
 
-    return <section className="guest-filter">
+    return (
+        <section className="guest-filter">
 
-        <div className="options">
-            <article className="option">
-                <div className="description">
-                    Adults
-                    <span>Ages 13 or above</span>
-                </div>
+            <div className="options">
 
-                <div className="count">
-                    <button className={filterByToEdit.guestCount.adults === 1 ? 'disabled' : ''} onClick={() => {
-                        if (filterByToEdit.guestCount.adults > 1) {
-                            updateGuestCounts('adults', -1)
-                        }
-                    }}>-</button>
-                    {filterByToEdit.guestCount.adults}
-                    <button className={filterByToEdit.guestCount.adults + filterByToEdit.guestCount.children >= 16 ? 'disabled' : ''} onClick={() => {
-                        if (filterByToEdit.guestCount.adults + filterByToEdit.guestCount.children < 16) {
-                            updateGuestCounts('adults', +1)
-                        }
-                    }}>+</button>
-                </div>
-            </article>
+                <article className="option">
+                    <div className="description">
+                        Adults
+                        <span>Age 18 or above</span>
+                    </div>
+                    <div className="count">
+                        <button className={headerFilterBy.guestCount.adults === 0 ? 'disabled' : ''} onClick={() => {
+                            if (headerFilterBy.guestCount.adults > 1) {
+                                updateGuestCounts('adults', -1)
+                            }
+                        }}>-</button>
+                        {headerFilterBy.guestCount.adults}
+                        <button className={headerFilterBy.guestCount.adults + headerFilterBy.guestCount.children >= 16 ? 'disabled' : ''} onClick={() => {
+                            if (headerFilterBy.guestCount.adults + headerFilterBy.guestCount.children < 16) {
+                                updateGuestCounts('adults', +1)
+                            }
+                        }}>+</button>
+                    </div>
+                </article>
+                <article className="option">
+                    <div className="description">
+                        Children
+                        <span>Ages 2 - 12</span>
+                    </div>
 
-            <article className="option">
-                <div className="description">
-                    Children
-                    <span>Ages 2 - 12</span>
-                </div>
-
-                <div className="count">
-                    <button className={filterByToEdit.guestCount.children === 0 ? 'disabled' : ''} onClick={() => {
-                        if (filterByToEdit.guestCount.children > 0) {
-                            updateGuestCounts('children', -1)
-                        }
-                    }}>-</button>
-                    {filterByToEdit.guestCount.children}
-                    <button className={filterByToEdit.guestCount.adults + filterByToEdit.guestCount.children >= 16 ? 'disabled' : ''} onClick={() => {
-                        if (filterByToEdit.guestCount.adults + filterByToEdit.guestCount.children < 16) {
-                            updateGuestCounts('children', +1)
-                        }
-                    }}>+</button>
-                </div>
-            </article>
-
-            <article className="option">
-                <div className="description">
-                    Infants
-                    <span>Under 2</span>
-                </div>
-
-                <div className="count">
-                    <button className={filterByToEdit.guestCount.infants === 0 ? 'disabled' : ''} onClick={() => {
-                        if (filterByToEdit.guestCount.infants > 0) {
-                            updateGuestCounts('infants', -1)
-                        }
-                    }}>-</button>
-                    {filterByToEdit.guestCount.infants}
-                    <button className={filterByToEdit.guestCount.infants === 5 ? 'disabled' : ''} onClick={() => {
-                        if (filterByToEdit.guestCount.infants < 5) {
-                            updateGuestCounts('infants', +1)
-                        }
-                    }}>+</button>
-                </div>
-            </article>
-
-            <article className="option">
-                <div className="description">
-                    Pets
-                </div>
-
-                <div className="count">
-                    <button className={filterByToEdit.guestCount.pets === 0 ? 'disabled' : ''} onClick={() => {
-                        if (filterByToEdit.guestCount.pets > 0) {
-                            updateGuestCounts('pets', -1)
-                        }
-                    }}>-</button>
-                    {filterByToEdit.guestCount.pets}
-                    <button className={filterByToEdit.guestCount.pets === 5 ? 'disabled' : ''} onClick={() => {
-                        if (filterByToEdit.guestCount.pets < 5) {
-                            updateGuestCounts('pets', +1)
-                        }
-                    }}>+</button>
-                </div>
-            </article>
-        </div>
-    </section>
+                    <div className="count">
+                        <button className={headerFilterBy.guestCount.children === 0 ? 'disabled' : ''} onClick={() => {
+                            if (headerFilterBy.guestCount.children > 0) {
+                                updateGuestCounts('children', -1)
+                            }
+                        }}>-</button>
+                        {headerFilterBy.guestCount.children}
+                        <button className={headerFilterBy.guestCount.adults + headerFilterBy.guestCount.children >= 16 ? 'disabled' : ''} onClick={() => {
+                            if (headerFilterBy.guestCount.adults + headerFilterBy.guestCount.children < 16) {
+                                updateGuestCounts('children', +1)
+                            }
+                        }}>+</button>
+                    </div>
+                </article>
+                <article className="option">
+                    <div className="description">
+                        Infants
+                        <span>Under 2</span>
+                    </div>
+                    <div className="count">
+                        <button className={headerFilterBy.guestCount.infants === 0 ? 'disabled' : ''} onClick={() => {
+                            if (headerFilterBy.guestCount.infants > 0) {
+                                updateGuestCounts('infants', -1)
+                            }
+                        }}>-</button>
+                        {headerFilterBy.guestCount.infants}
+                        <button className={headerFilterBy.guestCount.infants === 5 ? 'disabled' : ''} onClick={() => {
+                            if (headerFilterBy.guestCount.infants < 5) {
+                                updateGuestCounts('infants', +1)
+                            }
+                        }}>+</button>
+                    </div>
+                </article>
+                <article className="option">
+                    <div className="description">
+                        Pets
+                    </div>
+                    <div className="count">
+                        <button className={headerFilterBy.guestCount.pets === 0 ? 'disabled' : ''} onClick={() => {
+                            if (headerFilterBy.guestCount.pets > 0) {
+                                updateGuestCounts('pets', -1)
+                            }
+                        }}>-</button>
+                        {headerFilterBy.guestCount.pets}
+                        <button className={headerFilterBy.guestCount.pets === 5 ? 'disabled' : ''} onClick={() => {
+                            if (headerFilterBy.guestCount.pets < 5) {
+                                updateGuestCounts('pets', +1)
+                            }
+                        }}>+</button>
+                    </div>
+                </article>
+            </div>
+        </section>
+    )
 }
