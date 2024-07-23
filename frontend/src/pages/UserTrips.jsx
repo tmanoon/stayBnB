09720cc0696409659ceb392fb80c08e8a223b8fd
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-07-23 12:19:41 +0300

    Remove extra spaces in user trips component

--- frontend/src/pages/UserTrips.jsx
+++ frontend/src/pages/UserTrips.jsx
@@ -61,71 +61,33 @@ export function UserTrips() {
         }))
     }
 
-    if (!userTrips || !userTrips.length) return <section className='user-trips no-user-trips'>
-        <header>
-            <h1>Trips</h1>
-        </header>
+    if (!userTrips || !userTrips.length) return (
+        <section className='user-trips no-user-trips'>
+            <header>
+                <h1>Trips</h1>
+            </header>
 
-        <div>
-            <h2>No trips booked...yet!</h2>
-            <p>Time to dust off your bags and start planning your next adventure</p>
-            <button onClick={() => navigate('/')}>Start searching</button>
-        </div>
-
-        <p>Can't find your reservation here? <span>Visit the Help Center</span></p>
-    </section>
-
-    if (!trips || !trips.length) return <section className='user-trips no-user-trips'>
-        <header className='flex align-center space-between'>
-            <h1>Trips</h1>
-
-            <div className='filters flex'>
-                <select name="tense" value={tripFilter.tense} onChange={handleFilter}>
-                    <option value="all">All</option>
-                    <option value="future">Future</option>
-                    <option value="current">Current</option>
-                    <option value="past">Past</option>
-                </select>
-
-                <select name="status" value={tripFilter.status} onChange={handleFilter}>
-                    <option value="all">All</option>
-                    <option value="approved">Approved</option>
-                    <option value="pending">Pending</option>
-                    <option value="rejected">Rejected</option>
-                </select>
+            <div>
+                <h2>No trips booked...yet!</h2>
+                <p>Time to dust off your bags and start planning your next adventure</p>
+                <button onClick={() => navigate('/')}>Start searching</button>
             </div>
-        </header>
 
-        {tripFilter.tense === 'all' && <h2>All reservations</h2>}
-        {tripFilter.tense === 'future' && <h2>Upcoming reservations</h2>}
-        {tripFilter.tense === 'current' && <h2>Current reservations</h2>}
-        {tripFilter.tense === 'past' && <h2>Past reservations</h2>}
+            <p>Can't find your reservation here? <span>Visit the Help Center</span></p>
+        </section>
+    )
 
-        <div>
-            <h2>No trips to Show</h2>
-        </div>
-
-        <p>Can't find your reservation here? <span>Visit the Help Center</span></p>
-    </section>
-
-    return <>
-        <section className='user-trips'>
+    if (!trips || !trips.length) return (
+        <section className='user-trips no-user-trips'>
             <header className='flex align-center space-between'>
                 <h1>Trips</h1>
-
                 <div className='filters flex'>
-                    <div className='layout-btns flex align-center'>
-                        <button onClick={() => onLayout('lines')} className={`lines flex center ${(layoutType === 'lines' ? 'selected' : '')}`}></button>
-                        <button onClick={() => onLayout('cards')} className={`cards flex center ${(layoutType === 'cards' ? 'selected' : '')}`}></button>
-                    </div>
-
                     <select name="tense" value={tripFilter.tense} onChange={handleFilter}>
                         <option value="all">All</option>
                         <option value="future">Future</option>
                         <option value="current">Current</option>
                         <option value="past">Past</option>
                     </select>
-
                     <select name="status" value={tripFilter.status} onChange={handleFilter}>
                         <option value="all">All</option>
                         <option value="approved">Approved</option>
@@ -140,39 +102,73 @@ export function UserTrips() {
             {tripFilter.tense === 'current' && <h2>Current reservations</h2>}
             {tripFilter.tense === 'past' && <h2>Past reservations</h2>}
 
-            <ul className={`grid ${layoutType}`}>
-                {trips.map(trip => (
-                    <li key={trip._orderId} className={`trip-card grid ${trip.status}`} onClick={() => onChoose(trip)} >
-
-                        <div className='title flex column'>
-                            <h3>{trip.stay.name}</h3>
-                            <p><span>Booking number:</span>&nbsp;&nbsp;{trip._id.slice(18)}</p>
-                        </div>
-
-                        <div className='dates'>
-                            <h4>{utilService.timestampsToShortDates(+trip.entryDate, +trip.exitDate)}</h4>
-                        </div>
-
-                        <div className='address flex column'>
-                            <p>{trip.stay.location.address}</p>
-                            <p>{trip.stay.location.city}</p>
-                            <p>{trip.stay.location.country}</p>
-                        </div>
-
-                        <div className='image'>
-                            <img src={trip.stay.img} alt={trip.stay.name} />
-                            <p>{utilService.timestampDaysAway(+trip.entryDate, +trip.exitDate)} |
-                                <span className={`status ${trip.status}`}> {trip.status}</span>
-                            </p>
-                        </div>
-                    </li>
-                ))}
-            </ul>
-
+            <div>
+                <h2>No trips to Show</h2>
+            </div>
             <p>Can't find your reservation here? <span>Visit the Help Center</span></p>
         </section>
+    )
 
-        {onModal && chosenTrip && <TripModal trip={chosenTrip} setOnModal={setOnModal} />}
-        {/* <UserNotification /> */}
-    </>
+    return (
+        <>
+            <section className='user-trips'>
+                <header className='flex align-center space-between'>
+                    <h1>Trips</h1>
+                    <div className='filters flex'>
+                        <div className='layout-btns flex align-center'>
+                            <button onClick={() => onLayout('lines')} className={`lines flex center ${(layoutType === 'lines' ? 'selected' : '')}`}></button>
+                            <button onClick={() => onLayout('cards')} className={`cards flex center ${(layoutType === 'cards' ? 'selected' : '')}`}></button>
+                        </div>
+                        <select name="tense" value={tripFilter.tense} onChange={handleFilter}>
+                            <option value="all">All</option>
+                            <option value="future">Future</option>
+                            <option value="current">Current</option>
+                            <option value="past">Past</option>
+                        </select>
+                        <select name="status" value={tripFilter.status} onChange={handleFilter}>
+                            <option value="all">All</option>
+                            <option value="approved">Approved</option>
+                            <option value="pending">Pending</option>
+                            <option value="rejected">Rejected</option>
+                        </select>
+                    </div>
+                </header>
+
+                {tripFilter.tense === 'all' && <h2>All reservations</h2>}
+                {tripFilter.tense === 'future' && <h2>Upcoming reservations</h2>}
+                {tripFilter.tense === 'current' && <h2>Current reservations</h2>}
+                {tripFilter.tense === 'past' && <h2>Past reservations</h2>}
+
+                <ul className={`grid ${layoutType}`}>
+                    {trips.map(trip => (
+                        <li key={trip._orderId} className={`trip-card grid ${trip.status}`} onClick={() => onChoose(trip)} >
+                            <div className='title flex column'>
+                                <h3>{trip.stay.name}</h3>
+                                <p><span>Booking number:</span>&nbsp;&nbsp;{trip._id.slice(18)}</p>
+                            </div>
+                            <div className='dates'>
+                                <h4>{utilService.timestampsToShortDates(+trip.entryDate, +trip.exitDate)}</h4>
+                            </div>
+                            <div className='address flex column'>
+                                <p>{trip.stay.location.address}</p>
+                                <p>{trip.stay.location.city}</p>
+                                <p>{trip.stay.location.country}</p>
+                            </div>
+
+                            <div className='image'>
+                                <img src={trip.stay.img} alt={trip.stay.name} />
+                                <p>{utilService.timestampDaysAway(+trip.entryDate, +trip.exitDate)} |
+                                    <span className={`status ${trip.status}`}> {trip.status}</span>
+                                </p>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+                <p>Can't find your reservation here? <span>Visit the Help Center</span></p>
+            </section>
+
+            {onModal && chosenTrip && <TripModal trip={chosenTrip} setOnModal={setOnModal} />}
+            {/* <UserNotification /> */}
+        </>
+    )
 }
