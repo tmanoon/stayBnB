6e0fecc79c33f9639f8eb3fefd2cbefcffa273cb
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-07-24 00:40:13 +0300

    Add functionality to save btn in stay details component

--- frontend/src/pages/StayDetails.jsx
+++ frontend/src/pages/StayDetails.jsx
@@ -15,6 +15,8 @@ import { StayReviewsPreview } from "../cmps/StayDetailsCmps/StayReviewsPreview"
 import { Loading } from "../cmps/Loading"
 import { SvgPathCmp } from '../cmps/HelperCmps/SvgPathCmp'
 import { Accordion } from "../cmps/HelperCmps/Accordion"
+import { userService } from "../services/user.service"
+import { addStayToUserFavorites } from "../store/actions/user.actions"
 
 export function StayDetails() {
     const [searchParams, setSearchParams] = useSearchParams()
@@ -22,8 +24,8 @@ export function StayDetails() {
     const { isLoading } = useSelector(storeState => storeState.stayModule)
     const { stayId } = useParams()
     const { txt, adults, children, infants, pets, entryDate, exitDate } = Object.fromEntries(searchParams.entries())
-
-    const paramsFromFilter = { txt, adults, children, infants, pets, entryDate, exitDate}
+    const user = userService.getLoggedInUser()
+    const paramsFromFilter = { txt, adults, children, infants, pets, entryDate, exitDate }
 
     const [params, updateParams] = useState(paramsFromFilter)
     const [stay, setStay] = useState('')
@@ -55,6 +57,15 @@ export function StayDetails() {
         return hostName
     }
 
+    async function onSaveBtn() {
+        try {
+            if(!user.wishlist.find(_stay => _stay._id === stayId)) await addStayToUserFavorites(stayId)
+        } catch(err) {
+            console.log('err', err)
+            throw err
+        }
+    }
+
     return <>
         {isLoading && <Loading currentPage={'details'} />}
         {stay && <section className="stay-details">
@@ -65,7 +76,7 @@ export function StayDetails() {
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" aria-hidden="true" role="presentation" focusable="false" style={{ display: 'block', fill: 'none', height: '16px', width: '16px', stroke: 'currentcolor', strokeWidth: '2', overflow: 'visible' }}><g fill="none"><path d="M27 18v9a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2v-9M16 3v23V3zM6 13l9.3-9.3a1 1 0 0 1 1.4 0L26 13"></path></g></svg>
                         <span>Share</span>
                     </button>
-                    <button className="save-btn flex align-center">
+                    <button className="save-btn flex align-center" onClick={() => onSaveBtn()}>
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" aria-hidden="true" role="presentation" focusable="false" style={{ display: 'block', fill: 'none', height: '16px', width: '16px', stroke: 'currentcolor', strokeWidth: 2, overflow: 'visible' }}><path d="M16 28c7-4.73 14-10 14-17a6.98 6.98 0 0 0-7-7c-1.8 0-3.58.68-4.95 2.05L16 8.1l-2.05-2.05a6.98 6.98 0 0 0-9.9 0A6.98 6.98 0 0 0 2 11c0 7 7 12.27 14 17z"></path></svg>
                         <span>Save</span>
                     </button>
