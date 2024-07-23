d8364415127932631902d94dd6952597e46aac52
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-07-24 00:02:24 +0300

    Correct typo mistake in user reducer

--- frontend/src/store/reducers/user.reducer.js
+++ frontend/src/store/reducers/user.reducer.js
@@ -11,7 +11,7 @@ export const LOGOUT = 'LOGOUT'
 const initialState = {
     users: [],
     loggedInUser: '',
-    loggedInUserUserStays: [],
+    loggedInUserStays: [],
     userFavoriteStays: []
 }
 
