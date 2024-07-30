23495ad9568d3fbefd0e00acd89dd4296631d6d8
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-07-31 02:21:27 +0300

    RRemove extra spaces and change the order for readability purposes

--- frontend/src/services/filterLists.service.js
+++ frontend/src/services/filterLists.service.js
@@ -1,24 +1,24 @@
 export const filterLists = {
-    filterLabels: ['new', 'off the grid', 'iconic cities', 'rooms', 'creative spaces', 'boats', 'grand pianos', 'vineyards', 'historical homes', 'mansions', 'lake', 'bed and breakfasts', 'farms', 'windmills', 'towers', 'treehouses', 'skiing', 'earth homes', 'beach', 'amazing views', 'countryside', 'a frames', 'desert', 'design', 'beachfront', 'caves', 'national parks', 'castles', 'lakefront', 'islands', 'trulli', 'tropical', 'cabins', 'campers', 'camping', 'arctic', 'tiny homes', 'surfing', 'barns', 'cycladic homes', 'hanoks', 'ryokans', 'domes', 'shepard huts', 'yurts', 'minsus', 'casas particulares'],
-
+    filterLabels: ['new', 'off the grid', 'iconic cities', 'rooms', 'creative spaces', 'boats', 'grand pianos', 'vineyards', 'historical homes', 'mansions', 'lake',
+        'bed and breakfasts', 'farms', 'windmills', 'towers', 'treehouses', 'skiing', 'earth homes', 'beach', 'amazing views', 'countryside', 'a frames', 'desert', 'design',
+        'beachfront', 'caves', 'national parks', 'castles', 'lakefront', 'islands', 'trulli', 'tropical', 'cabins', 'campers', 'camping', 'arctic', 'tiny homes', 'surfing',
+        'barns', 'cycladic homes', 'hanoks', 'ryokans', 'domes', 'shepard huts', 'yurts', 'minsus', 'casas particulares'],
     placeTypeItems: [{ value: 'any', label: 'Any type' }, { value: 'room' }, { value: 'house', label: 'entire home' }],
     bbbItems: ['any', '1', '2', '3', '4', '5', '6', '7', '8+'],
     propTypeItems: ['house', 'apartment', 'guesthouse', 'hotel'],
-
     amenityEssentialsShown: ['wifi', 'kitchen', 'washer', 'dryer', 'air conditioning', 'heating'],
     amenityEssentialsHidden: ['dedicated workspace', 'tv', 'hair dryer', 'iron'],
     amenityFeatures: ['pool', 'hot tub', 'free parking', 'ev charger', 'crib', 'king bed', 'gym', 'BBQ grill', 'breakfast', 'indoor fireplace', 'smoking allowed'],
     amenityLocation: ['beachfront', 'waterfront'],
     amenitySafety: ['smoke alarm', 'carbon monoxide alarm', 'fire extinguisher', 'first aid kit'],
-
     hostLngsShown: ['english', 'french', 'german', 'japanese'],
-    hostLngsHidden: ['italian', 'russian', 'spanish', 'chinese (Simplified)', 'arabic', 'hindi', 'portuguese', 'turkish', 'dutch', 'korean', 'thai', 'greek', 'hebrew', 'polish', 'tagalog', 'danish', 'swedish', 'norwegian', 'finnish', 'czech', 'hungarian'],
-
+    hostLngsHidden: ['italian', 'russian', 'spanish', 'chinese (Simplified)', 'arabic', 'hindi', 'portuguese', 'turkish', 'dutch', 'korean', 'thai', 'greek', 'hebrew',
+        'polish', 'tagalog', 'danish', 'swedish', 'norwegian', 'finnish', 'czech', 'hungarian'],
     accessEntrance: ['Step-free guest entrance', 'Guest entrance wider than 32 inches', 'Accessible parking spot', 'Step-free path to the guest entrance'],
     accessBedrooms: ['Step-free bedroom access', 'Bedroom entrance wider than 32 inches'],
     accessBathrooms: ['Step-free bathroom access', 'Bathroom entrance wider than 32 inches', 'Shower grab bar', 'Toilet grab bar', 'Step-free shower', 'Shower or bath chair'],
     accessEquipment: ['Ceiling or mobile hoist'],
-
-    placeTypeItemsEdit: [{ value: 'house', label: 'An entire home', desc: 'Guests have the whole place to themselves.' }, { value: 'room', label: 'A room', desc: 'Guests have their own room in a home, plus access to shared spaces.' }, { value: 'shared', label: 'A shared room', desc: 'Guests sleep in a room or a common area that may be shared with you or others.' }],
-
+    placeTypeItemsEdit: [{ value: 'house', label: 'An entire home', desc: 'Guests have the whole place to themselves.' },
+    { value: 'room', label: 'A room', desc: 'Guests have their own room in a home, plus access to shared spaces.' },
+    { value: 'shared', label: 'A shared room', desc: 'Guests sleep in a room or a common area that may be shared with you or others.' }],
 }
