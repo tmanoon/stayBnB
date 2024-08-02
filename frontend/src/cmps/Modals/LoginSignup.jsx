46ee28034857d978d24bd20d9e71729b6d72e94a
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-08-02 22:11:33 +0300

    Edit structure of login signup to work property and fix typo mistakes

--- frontend/src/cmps/Modals/LoginSignup.jsx
+++ frontend/src/cmps/Modals/LoginSignup.jsx
@@ -3,13 +3,15 @@ import { userService } from "../../services/user.service"
 import { useState } from "react"
 
 export function LoginSignup({ setIsLoginModal }) {
+
     const [isSignup, setIsSignup] = useState(false)
     const [credentials, setCredentials] = useState(userService.getEmptyCredentials())
 
     async function onSubmitLoginSignup(ev) {
         try {
+            ev.preventDefault()
             const user = isSignup ? await signup(credentials) : await login(credentials)
-            if (user) onClose(ev)
+            if (user) onClose()
         } catch (err) {
             console.log(err)
             throw err
@@ -25,8 +27,8 @@ export function LoginSignup({ setIsLoginModal }) {
         setCredentials(prevCredentials => ({ ...prevCredentials, [id]: value }))
     }
 
-    function onClose(ev) {
-        ev.preventDefault()
+    function onClose() {
+        setIsSignup(false)
         setIsLoginModal(false)
     }
 
@@ -37,42 +39,41 @@ export function LoginSignup({ setIsLoginModal }) {
             <header className="flex column center">
                 <button className="back-btn flex center" onClick={onClose}></button>
                 <h1>Welcome to Staybnb</h1>
-                {isSignup && <p>Already signed up? <span onClick={onChangeLoginSignup}>Click here to login</span></p>}
-                {!isSignup && <p>Don't Have an account? <span onClick={onChangeLoginSignup}>Click here to sign up</span></p>}
+                {!isSignup && <p>Don't have an account? <span onClick={onChangeLoginSignup}>Click here to sign up</span></p>}
             </header>
 
             <form className="flex column" onSubmit={onSubmitLoginSignup}>
 
-                <label htmlFor="username" className="flex column space-between">Username:&nbsp;
+                <label htmlFor="username" className="flex column space-between">Username&nbsp;
                     <input id="username" type="text" value={credentials.username} onChange={onChangeField} required />
                 </label>
 
-                <label htmlFor="password" className="flex column space-between">Password:&nbsp;
+                <label htmlFor="password" className="flex column space-between">Password&nbsp;
                     <input id="password" type="password" value={credentials.password} onChange={onChangeField} required />
                 </label>
 
                 {isSignup && <>
-                    <label htmlFor="fullname" className="flex column space-between">Full name:&nbsp;
+                    <label htmlFor="fullname" className="flex column space-between">Fullname&nbsp;
                         <input id="fullname" type="text" value={credentials.fullname} onChange={onChangeField} required />
                     </label>
 
-                    <label htmlFor="about" className="flex column space-between">About:&nbsp;
+                    <label htmlFor="about" className="flex column space-between">About&nbsp;
                         <input id="about" type="text" value={credentials.about} onChange={onChangeField} required />
                     </label>
 
-                    <label htmlFor="location" className="flex column space-between">Location:&nbsp;
+                    <label htmlFor="location" className="flex column space-between">Location&nbsp;
                         <input id="location" type="text" value={credentials.location} onChange={onChangeField} required />
                     </label>
 
-                    <label htmlFor="gender" className="flex column space-between">Gender:&nbsp;
-                        <select id="gender" value={credentials.gender} onChange={onChangeField}>
+                    <label htmlFor="gender" className="flex column space-between">Gender&nbsp;
+                        <select id="gender" value={credentials.gender || 'female'} onChange={onChangeField}>
                             <option id="female" value="female">Female</option>
                             <option id="male" value="male">Male</option>
                             <option id="other" value="other">Other</option>
                         </select>
                     </label>
                 </>}
-                <button type="submit">Signup</button>
+                <button>{!isSignup? 'Login' : 'Signup'}</button>
             </form>
         </section>
     </>
