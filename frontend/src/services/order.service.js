67e0ef6ddd1ee932fb4ed73a97c3580252a6c0b7
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-08-03 00:09:12 +0300

    Edit function in order service

--- frontend/src/services/order.service.js
+++ frontend/src/services/order.service.js
@@ -44,10 +44,10 @@ async function getHostOrdersById(userId, sortBy) {
     try {
         store.dispatch({type: SET_IS_LOADING, isLoading: true})
         let orders = await query()
-        orders = orders.filter(order => order.hostId === userId).filter(order => order.entryDate > new Date())
+        orders = orders.filter(order => order.hostId === userId).filter(order => order.entryDate > new Date())        
+        store.dispatch({type: SET_IS_LOADING, isLoading: false})
         if (sortBy === 'date') return orders.sort((a,b) => a.entryDate - b.entryDate)
         if (sortBy === 'name') return orders.sort((a,b) => a.stay._id.localeCompare(b.stay._id))
-        store.dispatch({type: SET_IS_LOADING, isLoading: false})
         return orders
     } catch (err) {
         console.log(err)
