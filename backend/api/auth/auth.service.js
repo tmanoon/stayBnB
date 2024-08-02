16bb36f3a703940c094a6724e8c5ee8c93346bb4
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-08-02 22:10:19 +0300

    Edit function so invalid password will not pass in auth service backend

--- backend/api/auth/auth.service.js
+++ backend/api/auth/auth.service.js
@@ -17,27 +17,25 @@ async function login(username, password) {
     logger.debug(`auth.service - login with username: ${username}`)
 
     const user = await userService.getByUsername(username)
-    if (!user) throw 'Invalid username or password'
-    // TODO: un-comment for real login
-    // const match = await bcrypt.compare(password, user.password)
-    // if (!match) return Promise.reject('Invalid username or password')
-
+    if (!user) throw 'Invalid username'
+    const match = await bcrypt.compare(password, user.password)
+    if (!match) throw 'Invalid password'
     delete user.password
     user._id = user._id.toString()
     return user
 }
 
-async function signup({ username, password, fullname, imgUrl }) {
+async function signup({ username, password, fullname, imgUrl, location, about, gender }) {
     const saltRounds = 10
 
     logger.debug(`auth.service - signup with username: ${username}, fullname: ${fullname}`)
     if (!username || !password || !fullname) throw 'Missing required signup information'
 
-    const userExist = await userService.getByUsername(username)
-    if (userExist) throw 'Username already taken'
+    const userExists = await userService.getByUsername(username)
+    if (userExists) throw 'Username already taken'
 
     const hash = await bcrypt.hash(password, saltRounds)
-    return userService.add({ username, password: hash, fullname, imgUrl })
+    return userService.add({ username, password: hash, fullname, imgUrl, location, about, gender })
 }
 
 function getLoginToken(user) {
@@ -48,16 +46,10 @@ function getLoginToken(user) {
 function validateToken(loginToken) {
     try {
         const json = cryptr.decrypt(loginToken)
-        const loggedinUser = JSON.parse(json)
-        return loggedinUser
-
+        const loggedInUser = JSON.parse(json)
+        return loggedInUser
     } catch (err) {
         console.log('Invalid login token')
     }
     return null
 }
-
-// ;(async ()=>{
-//     await signup('bubu', '123', 'Bubu Bi')
-//     await signup('mumu', '123', 'Mumu Maha')
-// })()
