b02a4459077d8e5ad8dcc1df1567f385819d4c6c
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-08-04 11:01:03 +0300

    Remove unnecessary function from stay index page

--- frontend/src/pages/StayIndex.jsx
+++ frontend/src/pages/StayIndex.jsx
@@ -7,6 +7,7 @@ import { loadStays, setStayFilter } from '../store/actions/stay.actions.js'
 
 import { StayList } from '../cmps/StayList.jsx'
 import { Loading } from '../cmps/Loading.jsx'
+import { stayService } from '../services/stay.service.js'
 
 export function StayIndex({ scrolledPage }) {
     const [searchParams, setSearchParams] = useSearchParams()
@@ -16,7 +17,7 @@ export function StayIndex({ scrolledPage }) {
     const { isLoading } = useSelector(storeState => storeState.stayModule)
     const [user, setUser] = useState(null)
 
-    
+
     useEffect(() => {
         const user = userService.getLoggedInUser()
         setUser(user || null)
@@ -41,11 +42,28 @@ export function StayIndex({ scrolledPage }) {
         } else { return 'index-header-expanded' }
     }
 
+    const modifyHosts = async () => {
+        try {
+            const guestUser = await userService.getById('661e6cb61e29f39397b25fec')
+            const jennyUser = await userService.getById('661e801f1e29f39397b25fee')
+            const shovalUser = await userService.getById('661e820c1e29f39397b25ff1')
+            // const stays = await stayService.getAllStays()
+            // for(let i = 0; i < 4; i++) {
+            //     const randIdx = 
+            // }
+
+            console.log('done')
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
     return (
         <section className={`index-section ${scrolledHeader()}`}>
             {isLoading && <Loading />}
             {!isLoading &&
                 <>
+                    <button onClick={modifyHosts}>HEEEEY</button>
                     <StayList stays={stays} filterBy={filterBy} user={user} setUser={setUser} />
                     <section className='index-end-section flex column center'>
                         <h1>Continue exploring homes</h1>
