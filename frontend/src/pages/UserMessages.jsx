25431873f1571e44fded11f9f7d6b47b9fbc024a
JennyLottner <jenny.lottner@gmail.com>
2024-08-07 01:02:02 +0300

    Add space in UserMessages msg

--- frontend/src/pages/UserMessages.jsx
+++ frontend/src/pages/UserMessages.jsx
@@ -145,8 +145,8 @@ export function UserMessages() {
                 {currChat && currChat.msgs.length && <>
                     {currChat.msgs.map((msg, idx) =>
                         <li key={idx}  className={`flex column ${(user._id === msg.by)? 'user' : 'other'}`}>
-                            {(currChat.host._id === msg.by) && <div><span>{currChat.host.fullname.split(' ')[0]}</span>  &nbsp; &nbsp; {utilService.timestampToDateAndTimeObj(msg.at).shortDate} {utilService.timestampToDateAndTimeObj(msg.at).time}</div>}
-                            {(currChat.host._id !== msg.by) && <div><span>{currChat.buyer.fullname.split(' ')[0]}</span>  &nbsp; &nbsp; {utilService.timestampToDateAndTimeObj(msg.at).shortDate} {utilService.timestampToDateAndTimeObj(msg.at).time}</div>}
+                            {(currChat.host._id === msg.by) && <div><span>{currChat.host.fullname.split(' ')[0]}</span>  &nbsp; &nbsp; {utilService.timestampToDateAndTimeObj(msg.at).shortDate} &nbsp; {utilService.timestampToDateAndTimeObj(msg.at).time}</div>}
+                            {(currChat.host._id !== msg.by) && <div><span>{currChat.buyer.fullname.split(' ')[0]}</span>  &nbsp; &nbsp; {utilService.timestampToDateAndTimeObj(msg.at).shortDate} &nbsp; {utilService.timestampToDateAndTimeObj(msg.at).time}</div>}
                             <pre>{msg.txt}</pre>
                         </li>)}
                 </>}
