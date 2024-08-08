4eb2a8c5590368cbcb725d2b9352fd1aab37047d
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-08-08 23:46:30 +0300

    Remove unnecessary socket from socket service backend

--- backend/services/socket.service.js
+++ backend/services/socket.service.js
@@ -16,16 +16,6 @@ export function setupSocketAPI(server) {
             logger.info(`Socket disconnected [id: ${socket.id}]`)
         })
 
-        socket.on('chat-set-topic', topic => {
-            if (socket.myTopic === topic) return
-            if (socket.myTopic) {
-                socket.leave(socket.myTopic)
-                logger.info(`Socket is leaving topic ${socket.myTopic} [id: ${socket.id}]`)
-            }
-            socket.join(topic)
-            socket.myTopic = topic
-        })
-
         socket.on('chat-send-msg', msg => {
             logger.info(`New chat msg from socket [id: ${socket.id}], emitting to topic ${socket.myTopic}`)
             socket.broadcast.to(socket.myTopic).emit('chat-add-msg', msg)
@@ -63,7 +53,6 @@ export function setupSocketAPI(server) {
             if(!data[1]) emitTo({ type: 'prompt-notification', data: data[0], label: 'self' + socket.id })
             else await emitToUser({ type: 'prompt-notification', data: data[0], userId: data[1]})
         })
-
     })
 }
 
