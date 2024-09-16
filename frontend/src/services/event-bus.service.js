00fafceb89bbebeff155333be647f774a8e2b5fa
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-09-16 17:38:44 +0300

    Remove comments and add space in function for readability in event bus service

--- frontend/src/services/event-bus.service.js
+++ frontend/src/services/event-bus.service.js
@@ -2,15 +2,15 @@ function createEventEmitter() {
     const listenersMap = {}
     // Trick for DEBUG
     window.mapmap = listenersMap
+    
     return {
-        // Use this function to subscribe to an event
-        on(evName, listener){
+        on(evName, listener) {
             listenersMap[evName] = (listenersMap[evName])? [...listenersMap[evName], listener] : [listener]
             return ()=>{
                 listenersMap[evName] = listenersMap[evName].filter(func => func !== listener)
             }
         },
-        // Use this function to emit an event
+
         emit(evName, data) {
             if (!listenersMap[evName]) return
             listenersMap[evName].forEach(listener => listener(data))
